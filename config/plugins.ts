bb0749f6573ab3ff14cf20d85064866d96921164
Simon Belbeoch <simon.belbeoch@ln1.eu>
2025-02-21 20:18:32 +0100

    ajout du stockage s3

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+        config: {
+            provider: 'aws-s3',
+            providerOptions: {
+                credentials: {
+                    accessKeyId: env('S3_ACCESS_KEY_ID'),
+                    secretAccessKey: env('S3_ACCESS_SECRET'),
+                },
+                region: env('S3_REGION'), // e.g "fr-par"
+                endpoint: env('S3_ENDPOINT'), // e.g. "https://pwa-cms-s3-api.ln1.eu"
+                params: {
+                    Bucket: env('S3_BUCKET'),
+                },
+            },
+        },
+    },
+});
