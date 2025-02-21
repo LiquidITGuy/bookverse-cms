d5b95c48055ac34988497c219c72b753bfbe7edb
Simon Belbeoch <simon.belbeoch@ln1.eu>
2025-02-21 21:56:58 +0100

    test upload

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,4 +1,4 @@
-export default [
+module.exports = (env) => ([
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
@@ -9,4 +9,18 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://pwa-cms-s3.ln1.eu'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://pwa-cms-s3.ln1.eu'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+]);
--- config/plugins.ts
+++ config/plugins.ts
@@ -4,15 +4,19 @@ module.exports = ({ env }) => ({
         config: {
             provider: 'aws-s3',
             providerOptions: {
-                credentials: {
+                s3Options: {
                     accessKeyId: env('S3_ACCESS_KEY_ID'),
                     secretAccessKey: env('S3_ACCESS_SECRET'),
+                    region: env('S3_REGION'), // e.g "fr-par"
+                    s3ForcePathStyle: true,
+                    endpoint: env('S3_ENDPOINT'), // e.g. "https://pwa-cms-s3-api.ln1.eu"
+                    params: {
+                        Bucket: env('S3_BUCKET'),
+                        ACL: 'private', // <== set ACL to private
+                        signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+                    },
                 },
-                region: env('S3_REGION'), // e.g "fr-par"
-                endpoint: env('S3_ENDPOINT'), // e.g. "https://pwa-cms-s3-api.ln1.eu"
-                params: {
-                    Bucket: env('S3_BUCKET'),
-                },
+
             },
         },
     },
